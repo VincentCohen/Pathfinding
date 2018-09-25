aec1e494e3232a3e739bf4bd0251e85ca3238ae2
Vincent Cohen <v.cohen90@gmail.com>
2018-09-25 21:14:57 +0200

    Add grid

--- demo/Grid.js
+++ demo/Grid.js
@@ -1,26 +1,39 @@
 class Grid {
 
-    constructor(canvas) {
-
+    constructor() {
         this.width  = document.body.clientWidth;
         this.height = document.body.clientHeight;
 
+        var canvas = document.getElementById("grid");
         canvas.width = this.width;
         canvas.height = this.height;
+        
         this.canvas = canvas;
 
-        this.nodeSize  = 40;
+        this.ctx = canvas.getContext("2d");
+        this.ctx.strokeStyle = "#E3E3E3";
+
+        this.nodeSize  = 20;
         this.nodeColor = '#E3E3E3';
+
+        this.grid();
     }
   
-    generate() {
-        //
-        this.draw();
-    }
+    grid() {
 
-    draw() {
-        //
-        console.log(this.canvas);
+        for (var x = 0.5; x < this.canvas.width; x += this.nodeSize)
+        {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.canvas.width);
+        }
+
+        for (var y = 0.5; y < this.canvas.height; y += this.nodeSize)
+        {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.canvas.width, y);
+        }
+
+        return this.ctx.stroke();
     }
   
   }
