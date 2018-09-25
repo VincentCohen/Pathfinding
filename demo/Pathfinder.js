0ba8cb8b3ecbf06bad7c2b723c908b276052bf74
Vincent Cohen <v.cohen90@gmail.com>
2018-09-25 21:25:50 +0200

    wip

---
+++ demo/Pathfinder.js
@@ -0,0 +1,19 @@
+class Pathfinder {
+
+    constructor (canvas) {
+        let grid = new Grid(canvas);
+
+        // Run every x and redraw
+        this.update();
+    }
+
+    update () {
+        cells();
+    }
+
+    cells() {
+        // Draw every of every itteration
+    }
+
+}
+ 
