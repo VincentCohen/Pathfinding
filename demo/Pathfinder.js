f375e005e968cd93c50d51c35e34b37f22ad5b00
Vincent Cohen <v.cohen90@gmail.com>
2018-09-27 23:03:20 +0200

    wip.. trying out things here and there

--- demo/Pathfinder.js
+++ demo/Pathfinder.js
@@ -3,12 +3,22 @@ class Pathfinder {
     constructor (canvas) {
         let grid = new Grid(canvas);
 
+        this.addEventListeners()
+
         // Run every x and redraw
         this.update();
     }
 
+    addEventListeners() {
+        document.addEventListener("click", this.add)
+    }
+
+    add(event) {
+        console.log(event.clientX + '-' + event.clientY);
+    }
+
     update () {
-        cells();
+        this.cells();
     }
 
     cells() {
