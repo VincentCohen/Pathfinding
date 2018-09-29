04f847bb2fa9d381855c32c4e965786e2c3b8c03
Vincent <vincent@getseoshop.com>
2018-09-29 22:13:50 +0200

    Added ability to draw a square

--- demo/Grid.js
+++ demo/Grid.js
@@ -14,9 +14,28 @@ class Grid {
         this.ctx.strokeStyle = "#E3E3E3";
 
         this.nodeSize  = 20;
-        this.nodeColor = '#E3E3E3';
+        this.nodeColor = '#FF0000';
 
         this.grid();
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.fillStyle = this.nodeColor;
+        var x = 0;
+        for (var i = 0; i < 30; i++ ) {
+            this.square(i,x);
+            x++;
+        }
+    }
+
+    square(x,y) {
+        this.ctx.fillRect(
+            x*this.nodeSize, 
+            y*this.nodeSize, 
+            this.nodeSize, 
+            this.nodeSize
+        );
     }
   
     grid() {
@@ -27,11 +46,15 @@ class Grid {
             this.ctx.lineTo(x, this.canvas.width);
         }
 
+        console.log(Math.floor(x));
+
         for (var y = 0.5; y < this.canvas.height; y += this.nodeSize)
         {
             this.ctx.moveTo(0, y);
             this.ctx.lineTo(this.canvas.width, y);
         }
+
+        console.log(Math.floor(y));
 
         return this.ctx.stroke();
     }
